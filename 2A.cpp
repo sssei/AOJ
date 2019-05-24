0f2a93dbe5e60a16e8eca9c416d83396ecb90ca2
上野清雅 <seiga.ueno@gmail.com>
2019-05-24 19:52:19 +0900

    new file

---
+++ 2A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int  bubblesort(vector <int> vec, int n){
+  bool flag = true;
+  int cnt = 0;
+  while(flag){
+    flag = false;
+    for(int i = n-1; i > 0; i--){
+      if(vec.at(i) < vec.at(i-1)){
+	
+      }
+    }
+  }
+  
+}
+
+int main()
+{
+  
+}
