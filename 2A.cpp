c73d128bbf650c15a7b711da90a643d48e5d7a7b
上野清雅 <seiga.ueno@gmail.com>
2019-05-29 16:50:48 +0900

    new file

--- 2A.cpp
+++ 2A.cpp
@@ -3,21 +3,33 @@
 
 using namespace std;
 
-int  bubblesort(vector <int> vec, int n){
+int main(){
+  int N; cin >> N;
+  vector <int> vec(N);
+  for(int i = 0; i < N; i++){
+    cin >> vec.at(i);
+  }
+
   bool flag = true;
   int cnt = 0;
+  
   while(flag){
     flag = false;
-    for(int i = n-1; i > 0; i--){
-      if(vec.at(i) < vec.at(i-1)){
-	
+    for(int j = N-1; j > 0; j-- ){
+      if(vec.at(j) < vec.at(j-1)){
+	int temp = vec.at(j);
+	vec.at(j) = vec.at(j-1);
+	vec.at(j-1) = temp;
+	cnt++;
+	flag = true;
       }
     }
   }
-  
-}
 
-int main()
-{
+  for(int i = 0; i < N - 1; i++){
+    cout << vec.at(i) << " ";
+  }
+  cout << vec.at(N-1) << endl;
+  cout << cnt << endl;
   
 }
