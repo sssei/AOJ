4a8fa37949f40aceb64b587aa3655f34e64e48d2
上野清雅 <seiga.ueno@gmail.com>
2019-07-03 23:01:55 +0900

    new file

---
+++ 7A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#define MAX 1e5
+
+using namespace std;
+
+vector <int> D(MAX);
+vector <Node> T(MAX);
+
+Struct Node{
+  int parent, left, right;
+};
+
+void setDepth(int u,int p){
+  D.at(u) 
+}
