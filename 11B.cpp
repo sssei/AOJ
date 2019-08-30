6e32d5ce3e8f186b52f682f8566b276dc7e28b13
上野清雅 <seiga.ueno@gmail.com>
2019-08-30 13:32:00 +0900

    new file

--- 11B.cpp
+++ 11B.cpp
@@ -4,25 +4,62 @@
 #include <algorithm>
 #include <utility>
 #include <cmath>
-
+#define MAX 100
 using namespace std;
 
+enum color
+{
+ WHITE,
+ GLAY,
+ BLACK,
+};
+
+int n;
+int d[MAX], f[MAX];
+bool A[MAX][MAX];
+int t = 0;
+color color[MAX];
+
+void dfs(int u){
+  color[u] = GLAY;
+  d[u] = ++t;
+  for(int i = 0; i < n; ++i){
+    if(A[u][i] && color[i] == WHITE){
+      dfs(i);
+    }
+  }
+  color[u] = BLACK;
+  f[u] = ++t;
+}
+
 int main(){
-  int n; cin >> n;
-  vector< vector<int> > Adj(n,vector<int>(0));
-  vector<int> d(n);
-  vector<int> f(n);
+  cin >> n;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      A[i][j] = false;
+    }
+  }
+                                   
   for(int i = 0; i < n; i++){
     int id; cin >> id;
     int k; cin >> k;
     for(int j = 0; j < k; j++){
       int tmp; cin >> tmp;
-      Adj[i].push_back(tmp);
+      A[id-1][tmp-1] = true;
     }
   }
 
-  while(1){
-     
+  for(int i = 0; i < n; ++i){
+    color[i] = WHITE;
+  }
+
+  for(int i = 0; i < n; ++i){
+    if(color[i] == WHITE){
+      dfs(i);
+    }
+  }
+  for(int i = 0; i < n; ++i){
+    cout << i + 1 << " " << d[i] << " " << f[i] << endl;
   }
 }
 
